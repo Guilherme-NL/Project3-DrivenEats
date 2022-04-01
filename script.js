7b7d808cef93d8e3209b73213f1575872f47b682
Guilherme-NL <guilherme.nunes.lopes@gmail.com>
2022-04-01 11:18:03 -0300

    feat: add 'finalizar pedido' button

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ function selectPrato(element) {
   } else {
     element.classList.add("prato-selected");
   }
+  order();
 }
 function selectBebida(element) {
   const selectedBebida = document.querySelector(".bebida-selected");
@@ -13,6 +14,7 @@ function selectBebida(element) {
   } else {
     element.classList.add("bebida-selected");
   }
+  order();
 }
 function selectSobremesa(element) {
   const selectedSobremesa = document.querySelector(".sobremesa-selected");
@@ -20,5 +22,24 @@ function selectSobremesa(element) {
     selectedSobremesa.classList.remove("sobremesa-selected");
   } else {
     element.classList.add("sobremesa-selected");
+  }
+  order();
+}
+function order() {
+  const selectedSobremesa = document.querySelector(".sobremesa-selected");
+  const selectedBebida = document.querySelector(".bebida-selected");
+  const selectedPrato = document.querySelector(".prato-selected");
+  if (
+    selectedSobremesa !== null &&
+    selectedBebida !== null &&
+    selectedPrato !== null
+  ) {
+    const closeOrder = document.querySelector(".close-order-off");
+    closeOrder.innerHTML = "Fechar pedido";
+    closeOrder.classList.add("close-order-on");
+  } else {
+    const closeOrder = document.querySelector(".close-order-off");
+    closeOrder.innerHTML = "Selecione os 3 itens para fchar o pedido";
+    closeOrder.classList.remove("close-order-on");
   }
 }
