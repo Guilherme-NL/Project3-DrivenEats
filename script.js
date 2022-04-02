f27c444a9655947faa1a7f9d2cf2563a50cdfd9b
Guilherme-NL <guilherme.nunes.lopes@gmail.com>
2022-04-02 13:10:09 -0300

    feat: add order confirmation screen

--- script.js
+++ script.js
@@ -65,5 +65,60 @@ function price() {
   const price =
     selectedSobremesaPrice + selectedBebidaPrice + selectedPratoPrice;
   const priceFinal = price.toFixed(2);
-  alert(priceFinal);
+  console.log(priceFinal);
+}
+function closeOrder() {
+  const closeOrderOn = document.querySelector(".close-order-on");
+  if (closeOrderOn !== null) {
+    document.querySelector(".order").classList.remove("none");
+  }
+  //order name on confirmation screen
+  //prato selected name
+  const nomePrato = document.querySelector(".prato-selected > h3").innerHTML;
+  document.querySelector(".prato > h3").innerHTML = `${nomePrato}`;
+  //bebida selected name
+  const nomeBebida = document.querySelector(".bebida-selected > h3").innerHTML;
+  document.querySelector(".bebida > h3").innerHTML = `${nomeBebida}`;
+  //sobremesa selected name
+  const nomeSobremesa = document.querySelector(
+    ".sobremesa-selected > h3"
+  ).innerHTML;
+  document.querySelector(".sobremesa > h3").innerHTML = `${nomeSobremesa}`;
+  //order price on confirmation screen
+  //prato selected price
+  let selectedPratoPrice = document.querySelector(
+    ".prato-selected > h5"
+  ).innerHTML;
+  document.querySelector(".prato > h5").innerHTML = `${selectedPratoPrice}`;
+  //bebida selected price
+  let selectedBebidaPrice = document.querySelector(
+    ".bebida-selected > h5"
+  ).innerHTML;
+  document.querySelector(".bebida > h5").innerHTML = `${selectedBebidaPrice}`;
+  //sobremesa selected price
+  let selectedSobremesaPrice = document.querySelector(
+    ".sobremesa-selected > h5"
+  ).innerHTML;
+  document.querySelector(
+    ".sobremesa > h5"
+  ).innerHTML = `${selectedSobremesaPrice}`;
+  //TOTAL price
+  //calc
+  selectedSobremesaPrice = Number(
+    selectedSobremesaPrice.replace("R$ ", "").replace(",", ".")
+  );
+  selectedBebidaPrice = Number(
+    selectedBebidaPrice.replace("R$ ", "").replace(",", ".")
+  );
+  selectedPratoPrice = Number(
+    selectedPratoPrice.replace("R$ ", "").replace(",", ".")
+  );
+  const price =
+    selectedSobremesaPrice + selectedBebidaPrice + selectedPratoPrice;
+  const priceFinal = price.toFixed(2).replace(".", ",");
+  //define
+  document.querySelector(".total > h5").innerHTML = `R$ ${priceFinal}`;
+}
+function orderCancel() {
+  document.querySelector(".order").classList.add("none");
 }
