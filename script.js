12f7d0bc493d549d0c2505bd23014a1a07fdba16
Guilherme-NL <guilherme.nunes.lopes@gmail.com>
2022-04-10 16:13:10 -0300

    fix: double click to select in menu

--- script.js
+++ script.js
@@ -8,27 +8,24 @@ function selectPrato(element) {
   const selectedPrato = document.querySelector(".prato-selected");
   if (selectedPrato !== null) {
     selectedPrato.classList.remove("prato-selected");
-  } else {
-    element.classList.add("prato-selected");
   }
+  element.classList.add("prato-selected");
   order();
 }
 function selectBebida(element) {
   const selectedBebida = document.querySelector(".bebida-selected");
   if (selectedBebida !== null) {
     selectedBebida.classList.remove("bebida-selected");
-  } else {
-    element.classList.add("bebida-selected");
   }
+  element.classList.add("bebida-selected");
   order();
 }
 function selectSobremesa(element) {
   const selectedSobremesa = document.querySelector(".sobremesa-selected");
   if (selectedSobremesa !== null) {
     selectedSobremesa.classList.remove("sobremesa-selected");
-  } else {
-    element.classList.add("sobremesa-selected");
   }
+  element.classList.add("sobremesa-selected");
   order();
 }
 function order() {
